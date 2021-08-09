97c03bc03e8bd30c2dd764c2ac739a455ed400ee
sythong <sythongmta@gmail.com>
2021-08-09 15:04:11 +0700

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,8 +4,14 @@
 //   url:  "http://103.150.124.228:8080"
 // };
 
+// export const environment = {
+//   production: false,
+//   apiURL: "http://mltech-soft.com/api/",
+//   url: "http//localhost:4200/"
+// };
+
 export const environment = {
   production: false,
   apiURL: "http://mltech-soft.com/api/",
-  url: "http//localhost:4200/"
+  url:  "https://sythong.github.io/cardvisit_angular/"
 };
